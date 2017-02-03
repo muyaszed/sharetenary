e8f5f82f06f5696f9dde0cabd0c87a61a3b6bbae
yazed jamal <muyaszed@gmail.com>
2017-02-03 06:39:37 +0000

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131204645) do
+ActiveRecord::Schema.define(version: 20170201225309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "day"
+    t.text     "desc"
+    t.integer  "itenary_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["itenary_id"], name: "index_items_on_itenary_id", using: :btree
+  end
+
+  create_table "itenaries", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_itenaries_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'User creation' do
+
+	before do
+
+	end
+	
+
+	it "can be created" do
+		@user = User.create(username:"muyaszed", email: 'muyaszed@gmail.com', password: 'abcdef', password_confirmation: 'abcdef')
+		expect(@user).to be_valid
+	end
+
+	it "can't be created without username" do
+		@user = User.create(email: 'muyaszed@gmail.com',password: 'abcdef', password_confirmation: 'abcdef')
+		expect(@user).to_not be_valid
+	end 
+
+	it "can't be created without email" do
+		@user = User.create(username:"muyaszed",)
+		expect(@user).to_not be_valid
+	end 
+
+	it "has many itenaries" do
+		@user = User.create(username:"muyaszed", email: 'muyaszed@gmail.com', password: 'abcdef', password_confirmation: 'abcdef')
+		itenary1 = Itenary.create(title: "My Itenary", user_id: @user.id)
+		itenary2 = Itenary.create(title: "My Second Itenary", user_id: @user.id)
+		expect(@user.itenaries.count).to be > 1
+	end
+
 end
