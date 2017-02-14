b6bbabe08d62f4a797a9a57ed618d6b5792e5151
yazed jamal <muyaszed@gmail.com>
2017-02-14 20:31:23 +0000

    user can unfollow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,7 +5,17 @@ class FollowsController < ApplicationController
 		if current_user.follow @followed_user
 			
 	      respond_to do |format|
-	        format.html { redirect_to root_path }
+	        format.html { redirect_to user_path(params[:id]) }
+	        format.js
+	      end
+	    end
+	end
+
+	def unfollow_user
+		@followed_user = User.find(params[:id])
+		if current_user.unfollow @followed_user
+		  respond_to do |format|
+	        format.html { redirect_to user_path(params[:id]) }
 	        format.js
 	      end
 	    end
